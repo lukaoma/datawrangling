1ec758831024c2fbe3d2c7a45dcc4c375f4289f3
Larry <lukaoma@utexas.edu>
2020-12-05 21:53:03 -0800

    accident code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import csv
-import numpy as np
-import matplotlib.pyplot as plt
 import statistics
+import numpy as np
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def barGraph():
     plt.rcdefaults()
@@ -30,8 +32,25 @@ def barGraph():
     plt.show()
 
 
+def mapAccidnets():
+    df = pd.read_csv('AccidentsClean2018.csv')
+    limit = 5000
+    fig = go.Figure(data=go.Scattergeo(
+        lon=df['Start_Lng'][1:limit],
+        lat=df['Start_Lat'][1:limit],
+        text=df['Severity'][1:limit],
+        mode='markers',
+        marker_color='red',
+    ))
+
+    fig.update_layout(
+        title='Accidents<br>So Sad',
+        geo_scope='usa',
+    )
+    fig.show()
+
 
 if __name__ == '__main__':
-    barGraph()
+    mapAccidnets()
 
 
