58e5594f4bff93d9d70159d702597b853b0d41fe
Larry <lukaoma@utexas.edu>
2020-12-05 20:44:22 -0800

    first visualization

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics
 
-
-
-
-
-
-
-
+def barGraph():
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    genre = {}
+    with open('GamesClean2018.csv') as csvfile:
+        myCSVReader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+        for row in myCSVReader:
+            currentGenre = row['genre']
+            if row['genre'] not in genre:
+                genre[row['genre']] = []
+            genre[currentGenre].append(float(row["na_sales"])*10)
+    genreLables = []
+    averageSales = []
+    for singleGenre in genre:
+        if singleGenre not in "Sandbox":
+            genreLables.append(singleGenre)
+            averageSales.append(statistics.mean(genre[singleGenre]))
+    y_pos = np.arange(len(genreLables))
+    ax.barh(y_pos, averageSales, align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(genreLables)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel('Sales (millions)')
+    ax.set_title('How well did each genre do?')
+    plt.show()
 
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    barGraph()
+
 
